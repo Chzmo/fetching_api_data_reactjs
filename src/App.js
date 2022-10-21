eff9ecde2ef0ef71d4f9b07cf4fca18e3c412882
Chzmo <chisomomoyo10@gmail.com>
2022-10-21 11:19:13 +0200

    fetching movies

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import MovieCard from "./MovieCard";
 
 const API_URL = 'http://www.omdbapi.com/?apikey=bc183683'
 
-const movie = {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-}
-
 const App = () =>{
     const [movies, SetMovies] = useState([]);
 
@@ -51,7 +43,9 @@ const App = () =>{
                 movies.length > 0 ?
                 (
                     <div className="container">
-                        <MovieCard movie={movies[0]}/>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
                     </div>
                 ):(
                     <div className="empty">
